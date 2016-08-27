bfdaaef314efed39f05c95f23deca6def6301513
Dan Marshall <danmar@microsoft.com>
2016-08-26 17:43:26 -0700

    rename cancelAction

--- server.js
+++ server.js
@@ -233,7 +233,7 @@ bot.dialog('/prompts', [
         });
         session.endDialog(msg);
     }
-]).cancelAction('/menu', 'going back to the menu');
+]).cancelAction('/cancelprompts', 'going back to the menu');
 
 bot.dialog('/picture', [
     function (session) {
